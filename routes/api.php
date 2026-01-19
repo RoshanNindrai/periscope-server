664026d7bd60aa272800178c6b2c4c568a12413d
Roshan Nindrai <roshan.nindrai@gmail.com>
2026-01-19 14:24:11 -0500

    Fix nginx document root to point to public directory

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Test route to verify Laravel is working
+Route::get('/test', function () {
+    return response()->json(['status' => 'ok', 'message' => 'Laravel is working']);
+});
+
 // Authentication routes are handled by the Periscope\AuthModule package
 // See src/AuthModule/src/Providers/AuthModuleServiceProvider.php
